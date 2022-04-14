221ee133f99ae862729895e726ed28ae59a01c63
stay0nly <derkach.dmt@gmail.com>
2022-04-14 15:33:14 +0800

    App functionality

--- app.js
+++ app.js
@@ -1,28 +1,49 @@
-
-
+let color = "black";
+let click = true;
 
 function createBoard(size) {
-    let board = document.querySelector(".board");
-    let squres = board.querySelectorAll("div");
-    squres.forEach((div) => div.remove());
-    board.style.gridTemplateColums = `repeat(${size} , 1fr)`;
-    board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+  let board = document.querySelector(".board");
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
+  board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 
-    let amount = size * size;
-    
-    for (let i = 0; i < amount; i++) {
+  let amount = size * size;
+  for (let i = 0; i < amount; i++) {
     let square = document.createElement("div");
-    square.addEventListener("mouseover", function() {
-        square.style.backgroundColor = "black";
-    });
-    square.style.backgroundColor = "gray";
-    board.insertAdjacentElement("beforeend", square);   
-    }
+    square.addEventListener("mouseover", colorSquare);
+    square.style.backgroundColor = "white";
+    board.insertAdjacentElement("beforeend", square);
+  }
 }
 
 createBoard(16);
 
 function changeSize(input) {
     createBoard(input);
-    
+
 }
+
+function colorSquare() {
+  if (click) {
+  if (color === 'rainbow') {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  } else {
+    this.style.backgroundColor = color;
+  }
+}
+}
+
+function changeColor(choice) {
+  color = choice;
+}
+
+function resetBoard() {
+  let board = document.querySelector(".board");
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => div.style.backgroundColor = "white");
+}
+
+document.querySelector("body").addEventListener("click", function() {
+  click = !click;
+});
